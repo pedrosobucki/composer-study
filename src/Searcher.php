6f4a606d1165b25f358bf201a3b8f3740e3cc0e8
Pedro Sobucki <pedrosobucki@gmail.com>
2022-08-20 17:42:53 -0300

    Searcher class created for searching data based on URL received

---
+++ src/Searcher.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PSobucki\CourseSearcher;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Searcher
+{
+  
+    public function __construct(
+      private ClientInterface $httpClient,
+      private Crawler $crawler
+    ) { }
+
+    public function search(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $courseElements = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($courseElements as $element) {
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
+}
